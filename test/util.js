eaea6aed1035f5a470618fbd40674e8ac34479c2
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-05-20 18:03:50 +0200

    feat: add createWallet in utils tests

--- test/util.js
+++ test/util.js
@@ -1,11 +1,20 @@
 const { ethers } = require("hardhat");
-
+const crypto = require('crypto')
 module.exports = {
    makeSut: async (contract) => {
     const Sut = await ethers.getContractFactory(contract)
     const sut = await Sut.deploy()
     return {
       sut
+    }
+  },
+  makeWallet: () => {
+    const id = crypto.randomBytes(32).toString('hex')
+    const privateKey = '0x' + id
+    const wallet = new ethers.Wallet(privateKey)
+    return {
+      privateKey,
+      wallet
     }
   }
 }
