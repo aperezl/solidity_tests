6470aa1d5b26e906084fa552b32f6cccaec58023
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-05-22 13:40:56 +0200

    feat: add Error sample

---
+++ test/13.Error.js
@@ -0,0 +1,10 @@
+const { expect } = require("chai");
+const { makeSut } = require('./util')
+
+describe('Error ', () => {
+  
+  it('sum 5 should return 15', async () => {
+    // TODO: implement this test
+  })
+
+})
