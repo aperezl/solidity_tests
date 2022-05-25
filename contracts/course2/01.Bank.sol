4150177a9c1f43459d191c033bbc58231e5e88fd
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-05-25 23:31:01 +0200

    test: add test to Bank contract

--- contracts/course2/01.Bank.sol
+++ contracts/course2/01.Bank.sol
@@ -2,17 +2,34 @@
 pragma solidity ^0.8.7;
 
 contract Bank {
-  constructor() payable {}
+  address owner;
+
+  constructor() payable {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner {
+    require(msg.sender == owner, "not owner");
+    _;
+  }
 
   function incrementBalance(uint256 amount) payable public {
     require(msg.value == amount, "not match");
   }
 
-  function withdrawBalance() public {
+  function withdrawBalance() public onlyOwner {
     payable(msg.sender).transfer(address(this).balance);
   }
 
-  function b() public view returns (uint256) {
+  function newOwner(address _address) public onlyOwner {
+    owner = _address;
+  }
+
+  function getOwner() public view returns(address) {
+    return owner;
+  }
+
+  function getBalance() public view returns (uint256) {
     return address(this).balance;
   }
 }
--- test/course2/01.Bank.js
+++ test/course2/01.Bank.js
@@ -4,12 +4,34 @@ const { makeSut, getAccounts } = require('../util')
 
 
 describe('01.Bank', () => {
-  it('deploy contract', async () => {
+  it('ensure only owner can withdrawBalance', async () => {
     const { sut } = await makeSut('Bank')
     const [owner, addr1] = await getAccounts()
     const ether = ethers.utils.parseEther("10")
     const { gasPrice } = await sut.incrementBalance(ether, { value: ether })
-    await sut.connect(addr1).withdrawBalance()
+    await expect(sut.connect(addr1).withdrawBalance()).to.revertedWith('not owner')
+  })
+
+   it('ensure owner can withdrawBalance', async () => {
+    const { sut } = await makeSut('Bank')
+    const [owner, addr1] = await getAccounts()
+    const ether = ethers.utils.parseEther("10")
+    const { gasPrice } = await sut.incrementBalance(ether, { value: ether })
+    await expect(sut.withdrawBalance()).to.not.reverted
+  })
+
+  it('ensure only owner can call newOwner', async () => {
+    const { sut } = await makeSut('Bank')
+    const [owner, addr1] = await getAccounts()
+    await expect(sut.connect(addr1).newOwner(addr1.address)).to.revertedWith('not owner')
+    expect(await sut.getOwner()).to.equal(owner.address)
+  })
+
+  it('ensure owner can call newOwner', async () => {
+    const { sut } = await makeSut('Bank')
+    const [owner, addr1] = await getAccounts()
+    await expect(sut.newOwner(addr1.address)).to.not.reverted
+    expect(await sut.getOwner()).to.equal(addr1.address)
 
   })
 })
